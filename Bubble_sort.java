fbab7807bcd262db66e36fdb2dfb8e12a97418c1
Brajdeep Singh <brajdeepsingh8172@gmail.com>
2025-08-21 15:12:22 +0530

    First commit - merge sort program added

--- Bubble_sort.java
+++ Bubble_sort.java
@@ -4,26 +4,23 @@ public class Bubble_sort {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Get array size from user
         System.out.print("Enter the number of elements: ");
         int n = scanner.nextInt();
         
         int[] arr = new int[n];
         
-        // Get array elements from user
         System.out.println("Enter " + n + " elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
         
-        // Display original array
+
         System.out.println("\nOriginal array:");
         displayArray(arr);
         
-        // Perform bubble sort
+    
         bubbleSort(arr);
-        
-        // Display sorted array
+  
         System.out.println("\nSorted array (ascending order):");
         displayArray(arr);
         
@@ -34,14 +31,12 @@ public class Bubble_sort {
         int n = arr.length;
         boolean swapped;
         
-        // Outer loop for each pass
         for (int i = 0; i < n - 1; i++) {
             swapped = false;
-            
-            // Inner loop for comparing adjacent elements
+        
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // Swap arr[j] and arr[j+1]
+                   
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
@@ -49,7 +44,7 @@ public class Bubble_sort {
                 }
             }
             
-            // If no swapping occurred, array is already sorted
+           
             if (!swapped) {
                 break;
             }
